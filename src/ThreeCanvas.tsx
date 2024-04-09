1c28aa28db6e9211fbc19879ef07da9f8679b2f3
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-04-09 22:18:46 +0100

    Different path for prod and dev

--- src/ThreeCanvas.tsx
+++ src/ThreeCanvas.tsx
@@ -2,10 +2,8 @@ import { useRef, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as poly from './poly.json';
 
-const GLTF_FILE = '/poly.json';
+const GLTF_FILE = import.meta.env.DEV ? '/poly.json' : '/adr/poly.json';
 
 const Model = () => {
   const group = useRef(null);
